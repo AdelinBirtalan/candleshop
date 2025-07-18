35a5d5efb4d5c50bf68961f3670a066698accafc
unknown <adelinbirtalan3@gmail.com>
2025-07-18 14:19:50 +0300

    Adaugare navbar + design responsive navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import "./App.css";
-import NavBar from "./Components/NavBar/NavBar";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import NavBar from "./Components/NavBar/NavBar";
 import Home from "./Components/Home/Home";
 import Products from "./Components/Products/Products";
+import Contact from "./Components/Contact/Contact";
+import About from "./Components/About/About";
 
 function App() {
   return (
@@ -12,6 +14,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/products" element={<Products />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/about" element={<About />} />
         </Routes>
       </Router>
     </div>
--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,24 +1,45 @@
 import logo from "../../Media/logo.png"
+import "../NavBar/NavBar.css"
 import { Link } from "react-router-dom"
+import { FaShoppingCart } from "react-icons/fa";
+import { MdAccountCircle } from "react-icons/md";
+import { FaBars } from "react-icons/fa";
+import {useState} from "react"
+
+
+
 
 function NavBar() {
+
+    const [isPanelOpen, setIsPanelOpen] = useState(false)
+
     return (
+        <>
         <div className="navbar">
             <div className="logo-container">
                 <img src={logo} alt="logo"/>
                 <h2>Candley</h2>
             </div>
             <div className="links-container">
-                <Link to="/">Home</Link>
-                <Link to="/products">Products</Link>
+                <Link to="/" className="link-el">Home</Link>
+                <Link to="/products" className="link-el">Products</Link>
+                <Link to="/contact" className="link-el">Contact</Link>
+                <Link to="/about" className="link-el">About</Link>
             </div>
             <div className="cart-container">
-
+                <FaShoppingCart className="shop-icon"/>
+                <MdAccountCircle className="shop-icon"/>
+                <button onClick={() => setIsPanelOpen(!isPanelOpen)} className="bars-btn"><FaBars className="list-icon"/></button>
             </div>
         </div>
+        <div className={`side-panel ${isPanelOpen ? 'open' : ''}`}>
+            <Link to="/" className="link-el">Home</Link>
+                <Link to="/products" className="link-el">Products</Link>
+                <Link to="/contact" className="link-el">Contact</Link>
+                <Link to="/about" className="link-el">About</Link>
+        </div>
+        </>
     )
 }
 
 export default NavBar
-
-
