2776b255643b8348b4639c1f5675fceff71394f3
unknown <adelinbirtalan3@gmail.com>
2025-08-06 11:34:12 +0300

    Adaugare schimbari homepage

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -3,6 +3,7 @@ import { FaCreditCard, FaBox } from "react-icons/fa";
 import { TbTruckDelivery } from "react-icons/tb";
 import { CiHeart } from "react-icons/ci";
 import products from '../../Products';
+import infoPhoto from "../../Media/info-photo.jpg"
 
 
 function Home() {
@@ -28,13 +29,13 @@ function Home() {
                 </div>
             <div className="icon-box">
                 <FaBox className='icon-box-el'/>
-                <h2>Delivered with Care</h2>
-                <h3>Delivered to your home with care</h3>
+                <h2>Best Delivery</h2>
+                <h3>Fast delivery to your home</h3>
                 </div>
             <div className="icon-box">
                 <CiHeart className='icon-box-el'/>
                 <h2>Excellent Service</h2>
-                <h3>Excellent service from our team</h3>
+                <h3>Excellent service team</h3>
                 </div>
         </div>
 
@@ -48,6 +49,49 @@ function Home() {
                         <p>${product.price.toFixed(2)}</p>
                     </div>
                 ))}
+            </div>
+        </div>
+
+        <div className="discount-section">
+            <div className="discount-content">
+                <h2>Sale Now: Up to 40% Off every item</h2>
+                <h4>Dont miss out on our sale! For a limited time all items are at 40% discount!</h4>
+                <button>Shop Now</button>
+            </div>
+        </div>
+
+        <div className="info-section">
+            <div className="image-el">
+            <img src={infoPhoto} alt="CandlePhoto" />
+            </div>
+            <div className="text-el">
+            <h2>Lighting the Way to Calm,<br /> Naturally.</h2>
+            <p>
+  At Candly, we believe that the simple act of lighting a candle<br />
+  can transform your space—and your state of mind.<br />
+  In a world that moves fast and feels loud,<br />
+  our mission is to slow things down.<br />
+  Every candle we pour is designed with intention:<br />
+  clean-burning, nature-inspired,<br />
+  and hand-poured using sustainable ingredients<br />
+  that are gentle on you and the planet.<br />
+  <br />
+  From soothing lavender fields<br />
+  to cozy amber woods,<br />
+  our fragrances are curated<br />
+  to ground you in the present moment.<br />
+
+</p>
+                </div>
+        </div>
+
+        <div className="newsletter-section">
+            <div className="newsletter-text">
+            <h2>Newsletter Signup</h2>
+            <p>If you want to stay connected with our work subscribe to our newsletter!</p>
+            </div>
+            <div className="newsletter-btn">
+            <button>Get Updates</button>
             </div>
         </div>
         </>
